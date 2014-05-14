bca5ec23979898beea299c68b9d5315dbf2d60ea
Wisse <wisse.barkhof@student.auc.nl>
2014-05-14 09:45:56 +0200

    Evaluation of atoms.
    Passes test 1 - 3.

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -4,6 +4,7 @@ from types import Environment, LispError, Closure
 from ast import is_boolean, is_atom, is_symbol, is_list, is_closure, is_integer
 from asserts import assert_exp_length, assert_valid_definition, assert_boolean
 from parser import unparse
+import re as re
 
 """
 This is the Evaluator module. The `evaluate` function below is the heart
@@ -14,6 +15,22 @@ making your work a bit easier. (We're supposed to get through this thing
 in a day, after all.)
 """
 
+integer = re.compile('[0-9]+')
+symbol = re.compile('[a-zA-Z*<>+=-]+')
+
+
+
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
-    raise NotImplementedError("DIY")
+
+    # evaluating booleans
+    if is_boolean(ast):
+        return ast
+    if is_integer(ast):
+        return ast
+    if ast[0] == "quote":
+        return ast[1]
+
+
+
+
